8f9ad2f96135e07c6d6bcb7686433988063e4c39
OtherStream <OtherStream9042@gmail.com>
2025-09-19 22:26:00 -0600

    se le agrego que no apareciera nada si no habia vasos y si habia 10 vasos el boton de desabilitara

--- app/src/main/java/com/example/jeatpackcompose/WaterCounter.kt
+++ app/src/main/java/com/example/jeatpackcompose/WaterCounter.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -19,8 +18,10 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
