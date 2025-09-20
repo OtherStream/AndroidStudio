4abc73958db5908343a14e436eabcae32825ba49
OtherStream <OtherStream9042@gmail.com>
2025-09-19 22:36:48 -0600

    8. Cómo restablecer el estado en Compose

--- app/src/main/java/com/example/jeatpackcompose/WaterCounter.kt
+++ app/src/main/java/com/example/jeatpackcompose/WaterCounter.kt
@@ -12,10 +12,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.saveable.rememberSaveable
 
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable  { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+/*fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
@@ -40,4 +52,4 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             }
         }
     }
-}
+}*/
