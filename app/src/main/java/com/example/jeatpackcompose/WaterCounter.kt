519e8769b63803352e7c6b7ee9c78cf21e6fecc8
OtherStream <OtherStream9042@gmail.com>
2025-09-19 22:50:36 -0600

    paso 9

--- app/src/main/java/com/example/jeatpackcompose/WaterCounter.kt
+++ app/src/main/java/com/example/jeatpackcompose/WaterCounter.kt
@@ -15,7 +15,29 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.saveable.rememberSaveable
 
 
+
 @Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter() {
+    var waterCount by remember { mutableStateOf(0) }
+    var juiceCount by remember { mutableStateOf(0) }
+
+    StatelessCounter(waterCount, { waterCount++ })
+    StatelessCounter(juiceCount, { juiceCount++ })
+}
+
+/* funcion completa antes del paso 9
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by rememberSaveable  { mutableStateOf(0) }
@@ -26,7 +48,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             Text("Add one")
         }
     }
-}
+}*/
 /*fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
